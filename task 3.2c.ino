d2ab381473f24d4fb2ba9473a152254a4e35973f
PragatiKuchhal <131363196+PragatiKuchhal@users.noreply.github.com>
2023-10-27 10:44:15 +0530

    Update task 3.2c.ino

--- task 3.2c.ino
+++ task 3.2c.ino
@@ -1,57 +1,86 @@
-#include <hp_BH1750.h>
+#include <WiFiNINA.h>
 
-// Define the threshold for detecting sunlight
-const float THRESHOLD = 1000.0; // You should adjust this value based on your needs
+#include <BH1750.h>
+#include <Wire.h>
 
-bool sunlight = false; // Variable to track sunlight status
+//please enter your sensitive data in the Secret tab
+char ssid[] = "Redmi Note 10 Pro Max";
+char pass[] = "123456789";
 
-void onSunlightChange() {
-    // Handle sunlight change logic here
-    // You might want to implement specific actions based on sunlight detection
-    // For example, you could control external devices or send data to a server.
-}
+WiFiClient client;
+BH1750 lightMeter;
 
-void onLightSensorChange() {
-    float lightSensor = readLightIntensity(); // You should have a function to read light intensity
-
-    if (lightSensor > THRESHOLD) {
-        if (!sunlight) {
-            sunlight = true;
-            onSunlightChange(); // Call the sunlight change function when sunlight is detected
-        }
-        digitalWrite(LED_BUILTIN, HIGH);
-        Serial.print("Light intensity: ");
-        Serial.print(lightSensor);
-        Serial.println("Sunlight: ON");
-    } else {
-        if (sunlight) {
-            sunlight = false;
-            onSunlightChange(); // Call the sunlight change function when sunlight is no longer detected
-        }
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.print("Light intensity: ");
-        Serial.print(lightSensor);
-        Serial.println("Sunlight: OFF");
-    }
-}
+char   HOST_NAME[] = "maker.ifttt.com";
+String PATH_NAME  = "https://maker.ifttt.com/trigger/lightsensor/with/key/djmTxByajgaXmwohDns7rvPK_Nsk553vDqSaO1saWzK";
+String queryString = "?value1=57&value2=25";
 
 void setup() {
-    // Initialize the BH1750 sensor and setup
-    // You should add code here to initialize the sensor, set its mode, etc.
+  // initialize WiFi connection
+  // WiFi.begin(ssid, pass);
+  Serial.begin(9600);
+Serial.print("START");
+  while (!Serial);
 
-    // Initialize Serial communication
-    Serial.begin(9600);
-    pinMode(LED_BUILTIN, OUTPUT);
+  // connect to web server on port 80:
+     WiFi.begin(ssid, pass);
+     Wire.begin();
+while(true)
+  {
+  if (client.connect(HOST_NAME, 80)) {
+    // if connected:
+    Serial.println("Connected to server");
+    break;
+  }
+  else {// if not connected:
+    Serial.println("connection failed");
+    
+  }
+  delay(500);
 }
-
+lightMeter.begin();
+Serial.println("Connected to server");
+}
 void loop() {
-    onLightSensorChange();
-    delay(1000); // Adjust the delay as needed
-}
+  Serial.print("START");
+  float lux = lightMeter.readLightLevel();
 
-float readLightIntensity() {
-    // Implement code here to read the light intensity from the BH1750 sensor
-    // You'll need to use the appropriate library functions to communicate with the sensor.
-    // Return the light intensity as a float value.
-    // Example: return sensor.readLightLevel();
+  // Update the queryString with the current lux value and a descriptive message
+  if (lux > 1000) {
+    queryString = "?value1=" + String(lux) + "&value2=It's very bright!";
+  } else if (lux > 500) {
+    queryString = "?value1=" + String(lux) + "&value2=It's quite bright.";
+  } else if (lux > 100) {
+    queryString = "?value1=" + String(lux) + "&value2=It's somewhat dim.";
+  } else if (lux > 10) {
+    queryString = "?value1=" + String(lux) + "&value2=It's quite dark.";
+  } else {
+    queryString = "?value1=" + String(lux) + "&value2=It's very dark!";
+  }
+
+  Serial.println(queryString);
+
+  if (lux > 500) {
+    // make an HTTP request:
+    // send HTTP header
+    client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
+    client.println("Host: " + String(HOST_NAME));
+    client.println("Connection: close");
+    client.println(); // end HTTP header
+
+    while (client.connected()) {
+      if (client.available()) {
+        // read an incoming byte from the server and print it to the serial monitor:
+        char c = client.read();
+        Serial.print(c);
+      }
+    }
+
+    // the server's disconnected, stop the client:
+    client.stop();
+    Serial.println();
+    Serial.println("Disconnected");
+  }
+  
+  queryString = "";
+  delay(3000);
 }
